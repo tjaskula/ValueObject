f33e8b1f5a9ace9fa576c96c240111fbc5dbb427
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-12-03 01:13:36 +0100

    Add valueObject code

---
+++ src/ValueObject.UnitTests/ValueObjectTests.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections;
+using FluentAssertions;
+using Xunit;
+
+namespace ValueObject.UnitTests
+{
+    public abstract class ValueObjectTests<T> where T : ValueObject<T>
+    {
+        protected abstract ValueObject<T> CreateValueObject();
+
+        protected abstract ValueObject<T> CreateDifferentValueObject();
+
+        [Fact]
+        public void Should_properly_report_equality_with_default()
+        {
+            T stub = default(T);
+            T stub2 = default(T);
+
+            stub.Should().Be(stub2);
+        }
+
+        [Fact(Skip = "Failing test while featue in progress")]
+        public void Should_properly_report_equality_with_hydrated_props()
+        {
+            ValueObject<T> stub = CreateValueObject();
+            ValueObject<T> stub2 = CreateValueObject();
+
+            stub.Should().Be(stub2);
+            stub.Should().NotBeSameAs(stub2);
+        }
+
+        [Fact]
+        public void Should_properly_report_equality_when_same_reference()
+        {
+            ValueObject<T> stub = CreateValueObject();
+
+            stub.Should().Be(stub);
+            stub.Should().BeSameAs(stub);
+        }
+
+        [Fact]
+        public void Should_properly_report_nonequality()
+        {
+            ValueObject<T> stub = CreateValueObject();
+            ValueObject<T> stub2 = CreateDifferentValueObject();
+
+            stub.Should().NotBe(stub2);
+            stub.Should().NotBeSameAs(stub2);
+        }
+
+        [Fact]
+        public void Should_properly_report_nonequality_when_not_same_type()
+        {
+            var stub = new Stub();
+            ValueObject<T> stub2 = CreateValueObject();
+
+            stub2.Equals(stub).Should().BeFalse();
+            stub.Equals(stub2).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Should_report_get_hash_code_different_if_different_values()
+        {
+            ValueObject<T> stub = CreateValueObject();
+            ValueObject<T> stub2 = CreateDifferentValueObject();
+
+            stub.GetHashCode().Should().NotBe(stub2.GetHashCode());
+        }
+
+        [Fact]
+        public void Should_report_get_hash_code_same_if_same_values()
+        {
+            ValueObject<T> stub = CreateValueObject();
+            ValueObject<T> stub2 = CreateValueObject();
+
+            stub.GetHashCode().Should().Be(stub2.GetHashCode());
+        }
+
+        [Fact]
+        public void Should_report_not_equal_if_obj_is_null()
+        {
+            ValueObject<T> stub = CreateValueObject();
+            (stub.Equals(null)).Should().BeFalse();
+        }
+
+        [Fact]
+        public void Should_compare_null_attributes_to_non_null_attributes()
+        {
+            var stub1 = new ValueObjectStub();
+            var stub2 = new ValueObjectStub();
+
+            stub1.Address = "123 Main";
+
+            stub1.Should().NotBe(stub2);
+            stub2.Should().NotBe(stub1);
+        }
+
+        [Fact]
+        public void Should_compare_null_dates_to_non_null_attributes()
+        {
+            var stub1 = new ValueObjectStub();
+            var stub2 = new ValueObjectStub();
+
+            stub1.Date = DateTime.Now;
+
+            stub1.Should().NotBe(stub2);
+            stub2.Should().NotBe(stub1);
+        }
+
+        [Fact]
+        public void Should_copy_one_object_to_another()
+        {
+            var initialObject = new ValueObjectStub
+                                {
+                                    Address = "Address",
+                                    Age = 18,
+                                    Name= "Name",
+                                    Date = DateTime.Now
+                                };
+
+            var destinationObject = new ValueObjectStub();
+
+            initialObject.CopyTo(destinationObject);
+
+            initialObject.Equals(destinationObject).Should().BeTrue();
+        }
+
+        protected class Stub { }
+    }
+
+    public class ValueObjectStub : ValueObject<ValueObjectStub>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public DateTime? Date { get; set; }
+        public IList Items { get; set; }
+    }
+
+    public class ValueObjectStubTester : ValueObjectTests<ValueObjectStub>
+    {
+        protected override ValueObject<ValueObjectStub> CreateValueObject()
+        {
+            return new ValueObjectStub { Address = "4", Age = 2, Name = null, Items = new [] {new [] {1, 2, 3} } };
+        }
+
+        protected override ValueObject<ValueObjectStub> CreateDifferentValueObject()
+        {
+            return new ValueObjectStub { Address = "1", Age = 2, Name = null, Items = new[] { 1, 2, 5 } };
+        }
+    }
+}
---
+++ src/ValueObject/ValueObject.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ValueObject
+{
+    /// <summary>
+	/// Base class for Value Objects. DDD concept.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	[Serializable]
+    public abstract class ValueObject<T> : IEquatable<T> where T : class
+    {
+        /// <summary>
+        /// Compares two Value Objects according to the values of all fields in objects.
+        /// </summary>
+        /// <param name="obj">Object to compare to.</param>
+        /// <returns>True if objects are considered equal.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            var other = obj as T;
+
+            return Equals(other);
+        }
+
+        /// <summary>
+        /// Returns hashcode value calculated according to the values of all fields in objects.        
+        /// </summary>
+        /// <returns>Hashcode value.</returns>
+        public override int GetHashCode()
+        {
+            IEnumerable<FieldInfo> fields = GetFields();
+
+            const int startValue = 17;
+            const int multiplier = 59;
+
+            int hashCode = startValue;
+
+            foreach (FieldInfo field in fields)
+            {
+                object value = field.GetValue(this);
+
+                if (value != null)
+                    hashCode = hashCode * multiplier + value.GetHashCode();
+            }
+
+            return hashCode;
+        }
+
+        /// <summary>
+        /// Compares two Value Objects according to the values of all fields in objects.
+        /// </summary>
+        /// <param name="other">Object to compare to.</param>
+        /// <returns>True if objects are considered equal.</returns>
+        public virtual bool Equals(T other)
+        {
+            if (other == null)
+                return false;
+
+            Type t = GetType();
+
+            FieldInfo[] fields = t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            foreach (FieldInfo field in fields)
+            {
+                object value1 = field.GetValue(other);
+                object value2 = field.GetValue(this);
+
+                if (value1 == null)
+                {
+                    if (value2 != null)
+                        return false;
+                }
+                else if (value2 == null)
+                {
+                    return false;
+                }
+                else if (typeof(DateTime).IsAssignableFrom(field.FieldType) ||
+                         typeof(DateTime?).IsAssignableFrom(field.FieldType))
+                {
+                    string dateString1 = ((DateTime)value1).ToLongDateString();
+                    string dateString2 = ((DateTime)value2).ToLongDateString();
+
+                    if (!dateString1.Equals(dateString2))
+                    {
+                        return false;
+                    }
+                }
+                else if (typeof(string).IsAssignableFrom(field.FieldType))
+                {
+                    string string1 = value1.ToString();
+                    string string2 = value2.ToString();
+
+                    if (!string1.Equals(string2, StringComparison.OrdinalIgnoreCase))
+                        return false;
+                }
+                else if (typeof(IEnumerable).IsAssignableFrom(field.FieldType))
+                {
+                    var col1 = value1 as IEnumerable;
+                    var col2 = value2 as IEnumerable;
+                    
+                    if (col1 == null || col2 == null)
+                        return false;
+
+                    var e1 = col1.GetEnumerator();
+                    var e2 = col2.GetEnumerator();
+                    while (e1.MoveNext() && e2.MoveNext())
+                    {
+                        var item1 = e1.Current;
+                        var item2 = e2.Current;
+
+                        if (!item1.Equals(item2))
+                            return false;
+                    }
+                }
+                else if (!value1.Equals(value2))
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Copies the content of public properties to the object passed by parameter.
+        /// </summary>
+        /// <param name="coptyTo">Object that will contain the copied data.</param>
+        public virtual void CopyTo(T coptyTo)
+        {
+            Type t = GetType();
+            PropertyInfo[] properties = t.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            foreach (PropertyInfo property in properties)
+            {
+                var valueToCopy = property.GetValue(this, null);
+                if (valueToCopy == null)
+                    continue;
+                if (property.CanWrite)
+                    property.SetValue(coptyTo, valueToCopy, null);
+            }
+        }
+
+        private IEnumerable<FieldInfo> GetFields()
+        {
+            Type t = GetType();
+
+            var fields = new List<FieldInfo>();
+
+            while (t != typeof(object))
+            {
+                fields.AddRange(t.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public));
+
+                t = t.BaseType;
+            }
+
+            return fields;
+        }
+
+        /// <summary>
+        /// Compares two value objects for equality.
+        /// </summary>
+        /// <param name="x">The first value object.</param>
+        /// <param name="y">The second value object.</param>
+        /// <returns>Returns true if two object are the same.</returns>
+        public static bool operator ==(ValueObject<T> x, ValueObject<T> y)
+        {
+            if (ReferenceEquals(x, null))
+                return ReferenceEquals(y, null);
+
+            return x.Equals(y);
+        }
+
+        /// <summary>
+        /// Compares two value objects for inequlaity.
+        /// </summary>
+        /// <param name="x">The first value object.</param>
+        /// <param name="y">The second value object.</param>
+        /// <returns>Returns true if two object are not the same.</returns>
+        public static bool operator !=(ValueObject<T> x, ValueObject<T> y)
+        {
+            return !(x == y);
+        }
+    }
+}
