771a8785e9e75a191c0cfb9a97bed375e677a815
Tomasz Jaskula <thomasv1000@hotmail.fr>
2017-12-04 23:37:50 +0100

    Add UT to UnorderedValueObjects

--- src/DomainBuildingBlocks.UnitTests/UnorderedValueObjectsTests.cs
+++ src/DomainBuildingBlocks.UnitTests/UnorderedValueObjectsTests.cs
@@ -8,6 +8,8 @@ namespace DomainBuildingBlocks.UnitTests
     {
         protected abstract IEquatable<UnorderedValueObjects<SimpleValueObjectStub>> CreateValueObjects();
         
+        protected abstract IEquatable<UnorderedValueObjects<SimpleValueObjectStub>> CreateDifferentValueObjects();
+        
         [Fact]
         public void Should_properly_report_equality()
         {
@@ -15,6 +17,16 @@ namespace DomainBuildingBlocks.UnitTests
             var stub2 = CreateValueObjects();
 
             stub.Should().Be(stub2);
+            stub.Should().NotBeSameAs(stub2);
+        }
+        
+        [Fact]
+        public void Should_properly_report_equality_when_same_reference()
+        {
+            var stub = CreateValueObjects();
+
+            stub.Should().Be(stub);
+            stub.Should().BeSameAs(stub);
         }
     }
 
@@ -44,6 +56,35 @@ namespace DomainBuildingBlocks.UnitTests
                     Age = 18,
                     Date = new DateTime(2017, 10, 30),
                     Name = "Bom"
+                }
+            };
+            return simpleValueObjectStubs;
+        }
+        
+        protected override IEquatable<UnorderedValueObjects<SimpleValueObjectStub>> CreateDifferentValueObjects()
+        {
+            var simpleValueObjectStubs = new UnorderedValueObjects<SimpleValueObjectStub>
+            {
+                new SimpleValueObjectStub
+                {
+                    Address = "2",
+                    Age = 23,
+                    Date = new DateTime(2017, 11, 30),
+                    Name = "Nom"
+                },
+                new SimpleValueObjectStub
+                {
+                    Address = "1",
+                    Age = 18,
+                    Date = new DateTime(2017, 10, 30),
+                    Name = "Bom"
+                },
+                new SimpleValueObjectStub
+                {
+                    Address = "3",
+                    Age = 70,
+                    Date = new DateTime(2017, 12, 30),
+                    Name = "Vom"
                 }
             };
             return simpleValueObjectStubs;
